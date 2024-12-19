4a05068129b0f1a94575024b472d75437bff5961
kartikjaiswal99 <kartik99jais@gmail.com>
2024-12-20 01:20:37 +0530

    models added

---
+++ store/models.py
@@ -0,0 +1,114 @@
+from django.db import models
+from django.conf import settings
+from uuid import uuid4
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    featured_product = models.ForeignKey(
+        'Product', on_delete=models.SET_NULL, blank=True, null=True, related_name='featured_in')
+
+    def __str__(self):
+        return self.name
+
+
+class Size(models.Model):
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE, related_name='sizes')
+    size = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.size
+
+
+class Product(models.Model):
+    category = models.ForeignKey(
+        'Category', on_delete=models.CASCADE, related_name='products')
+    name = models.CharField(max_length=255)
+    slug = models.SlugField()
+    description = models.TextField(null=True, blank=True)
+    details = models.TextField(null=True, blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    stock = models.PositiveIntegerField()
+    sizes = models.ManyToManyField(Size, related_name='products')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    
+class Customer(models.Model):
+    first_name = models.CharField(max_length=255)
+    last_name = models.CharField(max_length=255)
+    phone_number = models.CharField(max_length=10, blank=False, null=False)
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+       
+
+class Address(models.Model):
+    user = models.ForeignKey(
+        'Customer', related_name="addresses", on_delete=models.CASCADE)
+    full_name = models.CharField(max_length=255, blank=False)
+
+    country = models.CharField(max_length=100, blank=False)
+    apartment_address = models.CharField(max_length=100, blank=False)
+    street_address = models.CharField(max_length=100)
+    city = models.CharField(max_length=100, blank=False)
+    postal_code = models.CharField(max_length=20, blank=False)
+    state = models.CharField(max_length=100, blank=False)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+ 
+class Order(models.Model):
+    PAYMENT_STATUS_PENDING = 'P'
+    PAYMENT_STATUS_COMPLETE = 'C'
+    PAYMENT_STATUS_CHOICES = [
+        (PAYMENT_STATUS_PENDING, 'Pending'),
+        (PAYMENT_STATUS_COMPLETE, 'Complete'),
+    ]
+
+    placed_at = models.DateTimeField(auto_now_add=True)
+    payment_status = models.CharField(
+        max_length=1, choices=PAYMENT_STATUS_CHOICES, default=PAYMENT_STATUS_PENDING)
+    customer = models.ForeignKey('Customer', on_delete=models.PROTECT)
+    shipping_address = models.ForeignKey(
+        'Address', related_name="shipping_orders", on_delete=models.SET_NULL, blank=True, null=True)
+
+    @property
+    def get_grand_total(self):
+        items = self.items.all()
+        total = sum([item.quantity * item.product.unit_price for item in items])
+        return total
+    
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(
+        Order, related_name="order_items", on_delete=models.CASCADE
+    )
+    product = models.ForeignKey(
+        Product, related_name="product_orders", on_delete=models.CASCADE
+    )
+    quantity = models.PositiveSmallIntegerField()
+    unit_price = models.DecimalField(max_digits=9, decimal_places=2)
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+class Cart(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        'Cart', on_delete=models.CASCADE, related_name='items')
+    product = models.ForeignKey(
+        'Product', on_delete=models.CASCADE)
+    quantity = models.PositiveSmallIntegerField()
